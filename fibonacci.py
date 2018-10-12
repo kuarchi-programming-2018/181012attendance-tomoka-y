efc6dc20f21a386615626792a513ed7c37965b30
tomoka-y <43874245+tomoka-y@users.noreply.github.com>
2018-10-12 18:07:14 +0900

    演習追加です

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+def fib(n):
+    a, b = 0, 1
+    for i in range(n):
+        a,b = b, a + b
+    return b
+
+print([fib(i) for　i in range(2018)])
+
+
+    
+    
